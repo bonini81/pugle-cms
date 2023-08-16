29cb34f5eb619c1dbaa39e640769bff65b87a4fa
bonini81 <andresdominguez81@gmail.com>
2023-08-15 21:46:15 -0500

    Titles casi ready

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,9 +4,13 @@ const Hero = () => {
   return (
     <section className="hero-background-image">
       <div className="copy-text-styles">
-        <h3>Hola soy Msc. Andrés Domínguez Bonini</h3>
-        <h1>Desarrollador Front End con React y Wordpress y SEO</h1>
-        <h3>
+        <h3 className="subtitle-text-padding">
+          Hola soy Msc. Andrés Domínguez Bonini
+        </h3>
+        <h1 className="title-text-size">
+          Desarrollador Front End con React y Wordpress y SEO
+        </h1>
+        <h3 className="subtitle-text-padding">
           Desarrollo de Applicaciones Web, Paginas Web y Tiendas Online a medida
         </h3>
       </div>
