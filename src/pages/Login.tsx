25974e738c086aa5c2482ed22fb5eeae7df58618
bonini81 <andresdominguez81@gmail.com>
2023-11-21 09:16:00 -0500

    Dandole forma a los formularios con react hook forms

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,31 @@
+import { useForm } from "react-hook-form";
+
 import "../scss/Login.scss";
 import Button from "../components/Boton";
 import TextField from "../components/CoTextField";
 
-
 const Login = (): JSX.Element => {
+
+  const  { register, handleSubmit } = useForm();
+
   return (
     <section className="main-wrapper-login-box">
       <div className="login-box-styles">
         <h1>Login Pugle CMS</h1>
-        <TextField data-testid="username" label="Username" type="text" />
-        <TextField
-          data-testid="username"
-          label="Password"
-          type="password"
-          className="login-field-styles"
-        />
-        <div className="login-button-styles">
-          <Button variant="contained" data-testid="login">
-            Enviar
-          </Button>
-        </div>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <TextField data-testid="username" label="Username" type="text" />
+          <TextField
+            data-testid="username"
+            label="Password"
+            type="password"
+            className="login-field-styles"
+          />
+          <div className="login-button-styles">
+            <Button variant="contained" data-testid="login">
+              Enviar
+            </Button>
+          </div>
+        </form>
       </div>
     </section>
   );
