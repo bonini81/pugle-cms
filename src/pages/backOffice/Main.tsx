d3773785aeb4355749070f97a73ac8a470bd0846
bonini81 <andresdominguez81@gmail.com>
2023-12-21 11:45:31 -0500

    BackOffice sidebar taking shape

--- src/pages/backOffice/Main.tsx
+++ src/pages/backOffice/Main.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import Person2Icon from "@mui/icons-material/Person2";
+import SpeedIcon from "@mui/icons-material/Speed";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
@@ -29,12 +31,24 @@ const Main = (): JSX.Element => {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 12, sm: 12, md: 12 }}
         >
-          <Grid item xs={12} sm={6} md={4}>
+          <Grid item xs={12} sm={6} md={4} className="grid-item-padding">
             <aside className="aside-navbar-styles">
-              Hola, {dataStore.users.currentUser}
+              <div className="aside-navbar-styles__user">
+                <p>
+                  <SpeedIcon className="mui-icons-align" /> Pugle BackOffice
+                  Dashboard
+                </p>
+                <p>
+                  Hola, <Person2Icon className="mui-icons-align" />{" "}
+                  {dataStore.users.currentUser}
+                </p>
+              </div>
+              <div className="aside-navbar-styles__menu">
+                <p>Menu</p>
+              </div>
             </aside>
           </Grid>
-          <Grid item xs={12} sm={6} md={8}>
+          <Grid item xs={12} sm={6} md={8} className="grid-item-padding">
             <section className="section-wrrapper-styles">
               <div>Main</div>
             </section>
