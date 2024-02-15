0688d9206862230c66e307c0eecd0e348027cbec
bonini81 <andresdominguez81@gmail.com>
2024-02-14 22:19:43 -0500

    Creating the portfolio page

--- src/components/LayOutBackOffice/LayOutBackOffice.tsx
+++ src/components/LayOutBackOffice/LayOutBackOffice.tsx
@@ -1,4 +1,10 @@
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
+import Person2Icon from "@mui/icons-material/Person2";
+import SpeedIcon from "@mui/icons-material/Speed";
+import StorageIcon from "@mui/icons-material/Storage";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 
 import logo from "../../assets/header/freelance-desarrolloweb-seo.jpg";
 import footerText from "../../data/footerLinks.json";
@@ -24,10 +30,52 @@ const headerProps = {
 };
 
 const LayOutBackOffice = (): JSX.Element => {
+  const dataStorUsers = useSelector((state: any) => state.users);
+
   return (
     <>
       <Header {...headerProps} />
-      <Outlet />
+      <main className="main-wrapper-styles">
+        <Container>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 12, sm: 12, md: 12 }}
+          >
+            <Grid item xs={12} sm={6} md={4} className="grid-item-padding">
+              <aside className="aside-navbar-styles">
+                <div className="aside-navbar-styles__user">
+                  <p>
+                    <SpeedIcon className="mui-icons-align" /> Pugle BackOffice
+                    Dashboard
+                  </p>
+                  <p>
+                    Hola, <Person2Icon className="mui-icons-align" />{" "}
+                    {dataStorUsers.currentUser}
+                  </p>
+                </div>
+                <div className="aside-navbar-styles__menu">
+                  <h3 className="htitle-style-margin">
+                    <StorageIcon
+                      className="icon-db-margin"
+                      style={{ fontSize: "1.7rem" }}
+                    />
+                    Database Contexts
+                  </h3>
+                  <ul className="main-list-margin">
+                    <li className="list-item-decoration">About Me</li>
+                    <li className="list-item-decoration">Services</li>
+                    <li className="list-item-decoration">Portfolio</li>
+                  </ul>
+                </div>
+              </aside>
+            </Grid>
+            <Grid item xs={12} sm={6} md={8} className="grid-item-padding">
+              <Outlet />
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
       <Footer mainFooter={false} copyright={copyright} />
     </>
   );
